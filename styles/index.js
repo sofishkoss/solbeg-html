69fe555814837e84da5ce8245c9934b13d1a8d39
sofishkosss <145139511+sofishkosss@users.noreply.github.com>
2024-09-25 16:49:45 +0300

    25/9/24

---
+++ styles/index.js
@@ -0,0 +1,63 @@
+const data = [
+  {
+    id: '71ce9eac-e9b9-44f0-a342-9ff0b565f3b7',
+    name: 'Hotel Leopold',
+    city: 'Saint Petersburg',
+    country: 'Russia',
+    image :
+  },
+  {
+    id: 'aa560608-a879-48a7-80b6-deff2806b250',
+    name: 'Apartment Sunshine',
+    city: 'Santa  Cruz de Tenerife',
+    country: 'Spain',
+    image
+    ,
+  },
+  {
+    id: '1d88fefe-edf1-4cda-844a-babbf29bb2b3',
+    name: 'Villa Kunerad',
+    city: 'Vysokie Tatry',
+    country: 'Slowakia',
+    image
+    ,
+  },
+  {
+    id: 'a2bf824d-edd8-41f0-8b70-244334086ab4',
+    name: 'Hostel Friendship',
+    city: 'Berlin',
+    country: 'Germany',
+    image
+    ,
+  },
+  {
+    id: '4024535d-a498-4274-b7cb-f01ada962971',
+    name: 'Radisson Blu Hotel',
+    city: 'Kyiv',
+    country: 'Ukraine',
+    image
+    ,
+  },
+  {
+    id: 'e51e71f6-6baf-4493-b3ae-25dc27cdc238',
+    name: 'Paradise Hotel',
+    city: 'Guadalupe',
+    country: 'Mexico',
+    image
+
+  },
+  {
+    id: '87d2b966-2431-43f3-8c0d-2c8723474dfc',
+    name: 'Hotel Grindewald',
+    city: 'Interlaken',
+    country: 'Switzerland',
+    image
+  },
+  {
+    id: '190221c6-b18f-4dba-97de-e35f0e14c023',
+    name: 'The Andaman Resort',
+    city: 'Port Dickson',
+    country: 'Malaysia',
+    image
+  },
+];
